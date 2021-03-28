d072a9d6362064fd103883ba13ffd740f0073ee1
StardustJK <1115159016@QQ.com>
2021-03-28 16:17:58 +0800

    注销

--- app/src/main/java/com/bupt/sse/group7/covid19/LogOutActivity.java
+++ app/src/main/java/com/bupt/sse/group7/covid19/LogOutActivity.java
@@ -2,13 +2,35 @@ package com.bupt.sse.group7.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bupt.sse.group7.covid19.model.CurrentUser;
 
 public class LogOutActivity extends AppCompatActivity {
+    private Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_out);
+        btn_logout=findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("Current_User", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("userId", null);
+                editor.commit();
+                CurrentUser.setCurrentUser(null);
+                finish();
+                Toast.makeText(getApplicationContext(), "成功注销", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
